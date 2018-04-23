33b362a6416d2cdcff636ca56a0f2f5fe9c8387f
barsyk <tames98@gmail.com>
2018-04-23 13:52:03 +0700

    Work started

---
+++ Advanced-Portfolio/MultipleChoiceQuestion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Portfolio
+{
+    public enum CorrectChoise { A = 1, B, C, D }
+
+    public class MultipleChoiceQuestion
+    {
+        public string Question { get; set; }
+
+        public string Choise1 { get; set; }
+
+        public string Choise2 { get; set; }
+
+        public string Choise3 { get; set; }
+
+        public string Choise4 { get; set; }
+
+        private CorrectChoise CorrectChoise { get; set; }
+
+        public MultipleChoiceQuestion(string q,string c1, string c2, string c3, string c4, CorrectChoise cc)
+        {
+            Question = q;
+            Choise1 = c1;
+            Choise2 = c2;
+            Choise3 = c3;
+            Choise4 = c4;
+            CorrectChoise = cc;
+        }
+
+    }
+}
--- Advanced-Portfolio/Program.cs
+++ Advanced-Portfolio/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Advanced_Portfolio
@@ -10,6 +11,58 @@ namespace Advanced_Portfolio
     {
         static void Main(string[] args)
         {
+            
+            string section = "";
+            string head = $"Multiple Choise Exam{section}";
+            string line = "\n";
+
+            for (int i = 0; i < head.Length; i++)
+            {
+                line += "-";
+            }
+            
+            Console.WriteLine(head+line);
+            Console.WriteLine("Please enter:\n" +
+                              "\t1 - to create a new question,\n" +
+                              "\t2 - to display all questions,\n" +
+                              "\t3 - to edit a question,\n" +
+                              "\t4 - to delete a question,\n" +
+                              "\t5 - to import questions from a file,\n" +
+                              "\t6 - to export questions to a file,\n" +
+                              "\t7 - to start the exam,\n" +
+                              "\t8 - to mark the exam,\n" +
+                              "\t0 - to exit the program.\n");
+            Start:
+            var a = Console.ReadKey(true);
+            switch (a.KeyChar)
+            {
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    break;
+                case '5':
+                    break;
+                case '6':
+                    break;
+                case '7':
+                    break;
+                case '8':
+                    break;
+                case '0':
+                    break;
+                default:
+                    Console.WriteLine("Write a number");
+                    goto Start;
+                    break;
+
+
+            }
+            
+            Console.ReadKey();
         }
     }
 }
